3b2a42dc73b48f67264077e1f610f831a41c1a58
akhmadtaufik <akhmadtaufik1703@gmail.com>
2026-06-11 14:24:54 +0700

    refactor(rooms): use requirePropertyPermission helper instead of requirePropertyAccess - Replace requirePropertyAccess import and invocation with requirePropertyPermission in rooms GET handler - Replace requirePropertyAccess import and invocation with requirePropertyPermission in rooms POST handler

--- server/api/rooms/index.post.ts
+++ server/api/rooms/index.post.ts
@@ -1,5 +1,5 @@
 import { createRoom } from '../../services/room.service'
-import { requirePropertyAccess } from '../../utils/rbac'
+import { requirePropertyPermission } from '../../utils/rbac'
 import { apiSuccess } from '../../utils/response'
 
 export default defineEventHandler(async (event) => {
@@ -10,7 +10,7 @@ export default defineEventHandler(async (event) => {
     throw createError({ statusCode: 400, statusMessage: 'propertyId, roomNumber, and monthlyRate are required' })
   }
 
-  await requirePropertyAccess(event.context.user, propertyId)
+  await requirePropertyPermission(event.context.user, propertyId)
 
   const newRoom = await createRoom(event.context.user, propertyId, {
     roomNumber: body.roomNumber,
